d0f18dd458be6c204b9fd3cab4744c48069d982b
jzhao11 <1258339346@qq.com>
2019-11-20 23:05:59 -0800

    added selling view controller and tab bar controller

---
+++ BuySellApp/SellingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SellingViewController.swift
+//  BuySellApp
+//
+//  Created by Mac on 11/20/19.
+//  Copyright © 2019 Christian Zhao. All rights reserved.
+//
+
+import UIKit
+
+class SellingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.magenta
+    }
+}
