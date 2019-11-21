7bfba57fde6170582c57a9908f81430a40740024
jzhao11 <1258339346@qq.com>
2019-11-21 13:44:51 -0800

    created method for image upload

--- BuySellApp/SellingViewController.swift
+++ BuySellApp/SellingViewController.swift
@@ -7,11 +7,63 @@
 //
 
 import UIKit
+import Alamofire
 
 class SellingViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func postItem(_ sender: Any) {
+        let url = "http://127.0.0.1:8888/buysell/api/item/create"
+        let params = [
+            "user": "123",
+            "title": "Mac Mini"
+        ]
+        let image = imageView.image
+        upload(url: url, params: params, image: image)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.magenta
+        imageView.image = UIImage(named: "MacMini")
+    }
+    
+    func upload(url: String, params: [String: String]?, image: UIImage?
+//        , success: @escaping (_ response: Any?) -> (), failture : @escaping (_ error: Error) -> ()
+    ) {
+        Alamofire.upload(
+            multipartFormData: { multipartFormData in
+                guard
+                    let arrData = params,
+                    let imageData = image?.jpegData(compressionQuality: 1.0)
+                else {
+                    return
+                }
+                
+                for (key, value) in arrData {
+                    multipartFormData.append((value.data(using: String.Encoding.utf8)!), withName: key)
+                }
+                let formatter = DateFormatter()
+                formatter.dateFormat = "yyyyMMddHHmmss"
+                let fileName = formatter.string(from: Date()) + ".jpg"
+                multipartFormData.append(imageData, withName: "title_img", fileName: fileName, mimeType: "image/jpeg")
+            },
+            to: url,
+            headers: nil,
+            encodingCompletion: { encodingResult in
+                switch encodingResult {
+                    case .success(let upload, _, _):
+                        upload.responseJSON { response in
+                            print("response = \(response)")
+                            let result = response.result
+                            if result.isSuccess {
+//                                success(response.value)
+                            }
+                        }
+                    case .failure(let encodingError):
+//                        failture(encodingError)
+                        print(encodingError)
+                }
+            }
+        )
     }
 }
