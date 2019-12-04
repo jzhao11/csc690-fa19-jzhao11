ae11290a5f0370660354babe4ec9d650443941ea
jzhao11 <1258339346@qq.com>
2019-12-04 04:29:20 -0800

    created list view of items in user dashboard

---
+++ BuySellApp/MyItemsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MyItemsViewController.swift
+//  BuySellApp
+//
+//  Created by Mac on 12/4/19.
+//  Copyright © 2019 Christian Zhao. All rights reserved.
+//
+
+import UIKit
+
+class MyItemsViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.magenta
+    }
+}
