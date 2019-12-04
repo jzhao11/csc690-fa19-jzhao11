ae806f9803e14662f25d39b6cf53a88166f8d8df
jzhao11 <1258339346@qq.com>
2019-12-04 04:30:24 -0800

    added nav controller for user dashboard

--- BuySellApp/SignInViewController.swift
+++ BuySellApp/SignInViewController.swift
@@ -13,9 +13,15 @@ class SignInViewController: UIViewController {
     @IBOutlet weak var segment: UISegmentedControl!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    @IBAction func signIn(_ sender: Any) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        signInButton.backgroundColor = UIColor(red: 0x28 / 0xFF, green: 0xA7 / 0xFF, blue: 0x45 / 0xFF, alpha: 1.0)
+        signInButton.setTitleColor(UIColor.white, for: .normal)
         segment.selectedSegmentIndex = 0
     }
     
