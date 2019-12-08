409f7fa45b9e8063aa5a40cd4f59f6e8b7fccccc
jzhao11 <1258339346@qq.com>
2019-12-07 17:13:43 -0800

    created inheritance hierarchy to models

---
+++ BuySellApp/Models/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  BuySellApp
+//
+//  Created by Mac on 12/7/19.
+//  Copyright © 2019 Christian Zhao. All rights reserved.
+//
+
+import Foundation
---
+++ BuySellApp/Models/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  BuySellApp
+//
+//  Created by Mac on 12/7/19.
+//  Copyright © 2019 Christian Zhao. All rights reserved.
+//
+
+import Foundation
